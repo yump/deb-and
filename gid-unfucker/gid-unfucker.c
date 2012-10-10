12d9fec30fd8efcb5a65dcd46e85a9f21bd3bbf2
rhaley <rhaley@ghasthawk.lan>
2012-10-10 10:41:34 -0500

    Added GPL license and copyright notices on all files.

--- gid-unfucker/gid-unfucker.c
+++ gid-unfucker/gid-unfucker.c
@@ -1,4 +1,22 @@
-/* This here be under the GPL version 3 */
+/*
+ * Copyright 2012 Russell Haley
+ *
+ * This file is part of Deb-And.
+ * 
+ * Deb-And is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Deb-And is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Deb-And.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #include "./android_filesystem_config.h"
 #include <stdlib.h>
--- scripts/start-chroot.ash
+++ scripts/start-chroot.ash
@@ -1,5 +1,23 @@
 #!/system/xbin/ash
 
+# Copyright 2012 Russell Haley
+
+# This file is part of Deb-And.
+#
+# Deb-And is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Deb-And is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Deb-And.  If not, see <http://www.gnu.org/licenses/>.
+
+
 mountpoint="/data/debian/debian"
 image="/data/debian/debian.img"
 
